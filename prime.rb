129b96139edcba56eb1106c57146cf6a6271c112
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-24 00:51:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,26 +6,3 @@ def prime?(int)
 
 end
 
-def prime_2?(int)
-
-  if int <= 1
-    return false
-  else
-    arr = (2...int).to_a
-    arr.none? {|x| int % x == 0}
-  end
-end
-
-Benchmark.bm(27) do |bm|
-  bm.report('using ternary') do
-    iterations.times do
-      prime?(111156)
-    end
-  end
-
-  bm.report('using if-else') do
-    iterations.times do
-      prime_2?(111156)
-    end
-  end
-end
