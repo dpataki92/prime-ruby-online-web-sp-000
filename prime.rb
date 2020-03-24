bfa975818af024c29b0af425750ee0db8530b2db
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-24 00:34:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,3 +4,14 @@ def prime?(int)
   int <= 1 ? false : (2...int).to_a.none? {|x| int % x == 0}
 
 end
+
+def prime_2?(int)
+
+  if int <= 1 ? 
+    return false
+  else
+    arr = (2...int).to_a
+    arr.none? {|x| int % x == 0}
+  end
+end
+
